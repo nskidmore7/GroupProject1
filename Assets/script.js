fff92e180dfc67c594f2816acc27eb88c0c18afa
Aalim Ali <aalimali01@gmail.com>
2021-10-07 10:55:57 -0500

    save code

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,13 @@
-
-
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
   
+
+//   $('.carousel').carousel({
+//     padding: 200    
+// });
+// autoplay();
+// function autoplay() {
+//     $('.carousel').carousel('next');
+//     setTimeout(autoplay, 4500);
+// }
